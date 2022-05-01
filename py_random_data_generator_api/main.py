0945830ab0a7c28623c085990dcf647afe380a44
Paul Matthews <paul@paulmatthews.dev>
2022-05-01 00:48:44 -0500

    I think I have root_path

--- py_random_data_generator_api/main.py
+++ py_random_data_generator_api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from routers import random, deterministic
 
-app = FastAPI()
+app = FastAPI(root_path="/api)
 
 app.include_router(random.router)
 app.include_router(deterministic.router)
