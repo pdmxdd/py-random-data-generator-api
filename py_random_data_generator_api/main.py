94bae7d65a4b589c70269e9bca16ddaecd099641
Paul Matthews <paul@paulmatthews.dev>
2022-05-01 10:33:10 -0500

    revert the revert the revert ... the revert?

--- py_random_data_generator_api/main.py
+++ py_random_data_generator_api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from routers import random, deterministic
 
-app = FastAPI(root_path="/api")
+app = FastAPI()
 
 app.include_router(random.router)
 app.include_router(deterministic.router)
