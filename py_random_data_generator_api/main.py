f6076c366efadf3c14ec26da570a15595dada2cd
Paul Matthews <paul@paulmatthews.dev>
2022-05-01 10:46:09 -0500

    reconfigured openapi docs

--- py_random_data_generator_api/main.py
+++ py_random_data_generator_api/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 from routers import random, deterministic
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url = "/api/openapi.json",
+    docs_url = "/api/docs",
+    redoc_url = "/api/redoc"
+)
 
 app.include_router(random.router)
 app.include_router(deterministic.router)
